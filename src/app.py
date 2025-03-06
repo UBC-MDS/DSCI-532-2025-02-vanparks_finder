20457931184d1b82c313701e939dcc1b3a67c7f6
davyxuximin <77637130+davyxuximin@users.noreply.github.com>
2025-03-06 09:48:14 -0800

    Update src/app.py
    Co-authored-by: Timothy Singh <timo3447@gmail.com>

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from dash import Dash, html, dcc
+from dash import Dash, html, dcc, callback_context
 import dash_bootstrap_components as dbc
 import dash_leaflet as dl
 from components import bar_chart, map
