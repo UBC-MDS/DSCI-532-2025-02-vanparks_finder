7630dff79eb9497b4699fd089723eca91aca4e5d
shengjiayuu <jessicayujiajia@gmail.com>
2025-03-08 13:16:28 -0800

    Have all text fit on one screen without scrolling

--- src/app.py
+++ src/app.py
@@ -23,43 +23,70 @@ server = app.server
 
 
 # Define the layout with a map centered on Vancouver
-app.layout = dbc.Container(fluid=True, children=[
-    dbc.Row([
-        # Left Column: Filters (imported from components/filters.py)
-        dbc.Col(
-        width=3,
-        children=[
-            html.H1("Vanparks Finder"),
-            html.Label("Neighbourhood Name"),
-            neighbourhood_dropdown,
-            html.Label("Facility Type"),
-            facility_dropdown,
-            html.Label("Special Feature"),
-            special_feature_dropdown,
-            washrooms_checkbox,
-            park_info_modal,
-            avg_hectare_card,
-            num_parks_card,
-        ],
-    ),
+app.layout = dbc.Container(
+    fluid=True,
+    style={"padding": "20px"},
+    children=[
+        dbc.Row([
+            dbc.Col(
+                width=3,
+                children=[
+                    html.H1("Vanparks Finder"),
+                    html.Label("Neighbourhood Name"),
+                    neighbourhood_dropdown,
+                    html.Label("Facility Type"),
+                    facility_dropdown,
+                    html.Label("Special Feature"),
+                    special_feature_dropdown,
+                    washrooms_checkbox,
+                    park_info_modal,
+                    avg_hectare_card,
+                    num_parks_card
+                ]
+            ),
+            dbc.Col(
+                width=9,
+                children=[
+                    park_map,
+                    dbc.Row([
+                        dbc.Col(
+                            html.Div(
+                                bar_chart,
+                                style={
+                                    "width": "70%",
+                                    "display": "inline-block",
+                                    "verticalAlign": "top"
+                                }
+                            ),
+                            width=10
+                        ),
+                        dbc.Col(
+                            html.Div(
+                                dcc.Markdown(
+                                    """
+                                    This dashboard was made to help park enthusiasts find parks 
+                                    suited to their preferences in Vancouver. Created by Inder Khera, 
+                                    Timothy Singh, Ximin Xu, Shengjia Yu. The GitHub Repo can be found 
+                                    [here.](https://github.com/UBC-MDS/DSCI-532-2025-02-vanparks_finder)
+                                    Latest Deployment Date: 2025-02-28
+                                    """
+                                ),
+                                style={
+                                    "fontSize": "6.3px",
+                                    "textAlign": "left",
+                                    "marginTop": "60px",
+                                    "marginRight": "7px"
+                                }
+                            ),
+                            width=2
+                        )
+                    ])
+                ]
+            )
+        ])
+    ]
+)
 
-
-        # Right Column: Map + Bar Chart
-        dbc.Col(width=9, children=[
-            park_map,
-            bar_chart
-        ]),
-    ]),
-    dbc.Row([
-        dcc.Markdown("""
-            This dashboard was made to help park enthusiasts find parks suited to their preferences in Vancouver. 
-            Created by Inder Khera, Timothy Singh, Ximin Xu, Shengjia Yu.
-
-            The GitHub Repo can be found [here.](https://github.com/UBC-MDS/DSCI-532-2025-02-vanparks_finder) 
-            Latest Deployment Date: 2025-02-28
-        """)
-    ])
-], style={"padding": "20px"})
 
 
 
