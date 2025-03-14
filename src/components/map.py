0e9bf8fb4f3e8797ab80cf2f73d3b879f6ada415
Inderjit Khera <inder.khera@hotmail.com>
2025-03-13 19:21:48 -0700

    optimized killarney

--- src/components/map.py
+++ src/components/map.py
@@ -10,7 +10,7 @@ geo_location_dict = {
     'Hastings-Sunrise': {'zoom': 13.5, 'center': [49.2778156, -123.0422014]},
     'Kensington-Cedar Cottage': {'zoom': 13.71, 'center': [49.2471833, -123.0769395]},
     'Kerrisdale': {'zoom': 13.7, 'center': [49.221843, -123.16097]},
-    'Killarney': {'zoom': 13.78, 'center': [49.2180367, -123.0383941]},
+    'Killarney': {'zoom': 13.58, 'center': [49.2180367, -123.0383941]},
     'Kitsilano': {'zoom': 14.26, 'center': [49.2674605, -123.1642213]},
     'Marpole': {'zoom': 14.46, 'center': [49.2104717, -123.130635]},
     'Mount Pleasant': {'zoom': 14.63, 'center': [49.2647148, -123.0978001]},
